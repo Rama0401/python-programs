f4c055b0372c31a797a7b18fce8ddc1f7acce5b7
Rama0401 <72615241+Rama0401@users.noreply.github.com>
2020-10-09 21:37:09 +0530

    Add files via upload

---
+++ swapping of 2 numbers using 3rd variable.py
@@ -0,0 +1,14 @@
+#swapping of 2 numbers using 3rd variable
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+
+
+##enter number:4
+##enter number:5
+##5
+##4
