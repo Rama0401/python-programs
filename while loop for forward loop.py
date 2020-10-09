0cc661a03a10ac79da70fdbfce72348123c42958
Rama0401 <72615241+Rama0401@users.noreply.github.com>
2020-10-09 21:26:24 +0530

    Add files via upload

---
+++ while loop for forward loop.py
@@ -0,0 +1,19 @@
+#while loop for forward loop
+n=int(input("enter number:"))
+i=0
+while n>i:
+    print(i)
+    i+=1
+    
+        
+##enter number:10
+##0
+##1
+##2
+##3
+##4
+##5
+##6
+##7
+##8
+##9
