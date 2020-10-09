eb157e92110c1fc7338318bfde5f2a36b54f7998
Rama0401 <72615241+Rama0401@users.noreply.github.com>
2020-10-09 22:57:09 +0530

    Add files via upload

---
+++ all_operations in list.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    N = int(input())
+    list1 = []
+    for i in range(N):
+        x = input().split(" ")
+        command  = x[0]
+        if command == "insert":
+            list1.insert(int(x[1]),int(x[2]))
+        elif command == "print":
+            print(list1)
+        elif command == "remove":
+            list1.remove(int(x[1]))
+        elif command == "append":
+            list1.append(int(x[1]))
+        elif command == "sort":
+            list1.sort()
+        elif command == "pop":
+            
+                list1.pop()
+        elif command == "reverse":
+            list1.reverse()
