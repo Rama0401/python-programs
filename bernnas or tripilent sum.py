70f5288e194712e85ee07bbb9965fd5b4afd10b8
Rama0401 <72615241+Rama0401@users.noreply.github.com>
2020-10-16 22:57:38 +0530

    Add files via upload

---
+++ bernnas or tripilent sum.py
@@ -0,0 +1,16 @@
+##tripilent sum pr bernnes algorithm
+##array=[12,2,3,1,4,6,9]
+##array.sort()
+##s=25
+##for i in range(len(array)):
+##    p=s-array[i]
+##    j=i+1
+##    k=len(array)-1
+##    while j<k:
+##        if array[j]+array[k]==p:
+##            print(array[i],array[j],array[k])
+##            break
+##        elif array[j]+array[k]<s:
+##            j+=1
+##        else:
+##            k-=1
